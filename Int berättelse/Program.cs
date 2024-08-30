a2fc8d5b09ea04aa25becbc1de7502b9d8d9853c
Jacob Bergman <Jacob.Bergman@elev.ga.ntig.se>
2024-08-30 10:18:04 +0200

    hejseb

---
+++ Int berättelse/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string opt1; string opt2 ; string opt1v2 ; string opt2v2 ; string opt2v2v1 ; string opt2v2v2;
+opt1 = Console.ReadLine();
+if (opt1) == "hund"
