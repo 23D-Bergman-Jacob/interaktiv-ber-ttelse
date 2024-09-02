98221a1e6c7c1f3594b0a73f82879e4a3cf4e2aa
Jacob Bergman <Jacob.Bergman@elev.ga.ntig.se>
2024-09-02 12:20:02 +0200

    ny berättelse

--- Int berättelse/Program.cs
+++ Int berättelse/Program.cs
@@ -1,40 +1,90 @@
-﻿// // See https://aka.ms/new-console-template for more information
-// using System.Linq.Expressions;
+﻿// // // See https://aka.ms/new-console-template for more information
+// // using System.Linq.Expressions;
 
-// Console.WriteLine("Hello, World!");
-// string opt1; string opt2 ; string opt1v2 ; string opt2v2 ; string opt2v2v1 ; string opt2v2v2;
-// opt1 = Console.ReadLine();
-// int
-//  Console.WriteLine("hejsan");
-//  Console.ReadLine();
-//  for (int i = 0; i <= 10; i = i + 2) 
+// // Console.WriteLine("Hello, World!");
+// // string opt1; string opt2 ; string opt1v2 ; string opt2v2 ; string opt2v2v1 ; string opt2v2v2;
+// // opt1 = Console.ReadLine();
+// // int
+// //  Console.WriteLine("hejsan");
+// //  Console.ReadLine();
+// //  for (int i = 0; i <= 10; i = i + 2) 
+
+// // {
+// //   Console.WriteLine(i);
+// // }
+
+// while (1 ==1 )
+// {
+// Console.WriteLine("användarnamn");
+
+// string name = Console.ReadLine();
+
+// if (name.ToLower() == "Jacob" )
 
 // {
-//   Console.WriteLine(i);
+
+//   Console.WriteLine("loggad in");
+
+//   break;
+
 // }
+// else if (name == "Jacke006")
+// {System.Console.WriteLine("nja du kommer in");
+// break; } 
+// else
 
-while (1 ==1 )
-{
-Console.WriteLine("användarnamn");
+// { System.Console.WriteLine("nej");} }
 
-string name = Console.ReadLine();
+// //{} alt gr 7
 
-if (name.ToLower() == "Jacob")
+// Console.ReadLine();
+using System.Globalization;
+using System.Net.Sockets;
 
-{
+string opt1;
+string opt2;
+string klar;
+opt2 = "hjk";
+klar = "nej";
+System.Console.WriteLine("det var en gång en åsna. Åsnan Älskade  ");
+while (klar == "nej"){
 
-  Console.WriteLine("loggad in");
+System.Console.WriteLine("Grönsaker/åsenkött");
+opt1=Console.ReadLine();
+if (opt1.ToLower() == "grönsaker") {
+System.Console.WriteLine("Åsnan gillade grönsaker speciellt morötter men en dag hade åsnan käkat upp alla morötter så han skulle ut på");
+System.Console.WriteLine("äventyr/chilla");
 
-  break;
+opt2 = Console.ReadLine();}
+ else if (opt1.ToLower() == "åsnekött")
+ {
+
+System.Console.WriteLine("Den här åsnan var något av vad man kallar en lönnmördare när det kommer till andra åsner. hans största rädsla var att åsnepolisen skulle gräva i hans trädgård! Men åsnan var väldigt sugen på åsnekött ska han käka sin");
+System.Console.WriteLine("Bästa kompis/Föräldrar");
+opt2 =Console.ReadLine ();
+}
+
+if (opt2.ToLower() == "äventyr") {
+  System.Console.WriteLine("Åsnan gick därför ut på stan och snodde ett stånd med alla morötter en åsna kan vilja ha i livet!!");
+klar = "ja";
+break;
+}
+else if (opt2.ToLower() == "chilla") {System.Console.WriteLine("åsnan stannade därför hemma och chillade!");
+klar = "ja";
+
+break;
+}
+ 
+ if (opt2.ToLower() == "bästa kompis") {
+  System.Console.WriteLine("Åsnan tänkte på alla dom dumma saker Sven hade gjort mot honom och bestämde sig för att han skulle bli nästa veckas mat.");
+ klar = "ja";
+ break;}
+
+if (opt1.ToLower() == "föräldrar"){
+  System.Console.WriteLine("Skaffa hjälp!");
+  klar = "ja";
 
 }
-else if (name == "Jacke006")
-{System.Console.WriteLine("nja du kommer in");
-break; } 
-else
-
-{ System.Console.WriteLine("nej");} }
-
-//{} alt gr 7
+}
 
 Console.ReadLine();
--- Int berättelse/obj/Debug/net8.0/Int berättelse.AssemblyInfo.cs
+++ Int berättelse/obj/Debug/net8.0/Int berättelse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Int berättelse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e6f60574c3d19875aa13f21137ce6f1dd23f6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6103309047b8dac461f5afcb1cbb652826089f31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Int berättelse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Int berättelse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
